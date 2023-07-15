89d65bb9a8bceac4a8bf7bf0fe1723c896177357
HebbaElsayed <114788974+HebbaElsayed@users.noreply.github.com>
2023-07-15 18:52:52 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+const result=require('./calc')
+result.sum(4,2)
+result.sub(4,2)
+result.multi(4,2)
+result.div(4,2)
---
+++ calc.js
@@ -0,0 +1,23 @@
+
+function sum(a,b){
+    console.log(a+b)
+}
+
+function sub(a,b){
+    console.log(a-b)
+}
+
+function multi (a,b){
+    console.log(a*b)
+}
+
+function div(a,b){
+    console.log(a/b)
+}
+
+module.exports = {
+    sum:sum,
+    sub:sub,
+    multi:multi,
+    div:div
+}
